e47c3a9722588d02f716678b32aa38a5117c7f12
FarukKeskinsoy <133097328+codeaddev@users.noreply.github.com>
2024-02-26 02:56:15 +0300

    ...

--- src/app/(pages)/recover-password/RecoverPasswordForm/index.tsx
+++ src/app/(pages)/recover-password/RecoverPasswordForm/index.tsx
@@ -50,14 +50,8 @@ export const RecoverPasswordForm: React.FC = () => {
     <Fragment>
       {!success && (
         <React.Fragment>
-          <h1>Recover Password</h1>
-          <div className={classes.formWrapper}>
-            <p>
-              {`Please enter your email below. You will receive an email message with instructions on
-              how to reset your password. To manage your all users, `}
-              <Link href="/admin/collections/users">login to the admin dashboard</Link>
-              {'.'}
-            </p>
+          <p>Kayıtlı e-posta adresinizi girdiğinizde şifrenizi yenilemek için kod gönderilecektir.</p>
+          
             <form onSubmit={handleSubmit(onSubmit)} className={classes.form}>
               <Message error={error} className={classes.message} />
               <Input
@@ -71,11 +65,10 @@ export const RecoverPasswordForm: React.FC = () => {
               <Button
                 type="submit"
                 appearance="primary"
-                label="Recover Password"
+                label="Gönder"
                 className={classes.submit}
               />
             </form>
-          </div>
         </React.Fragment>
       )}
       {success && (
